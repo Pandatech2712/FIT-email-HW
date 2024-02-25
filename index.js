fca8053509b3146ba8a0de904b59d0b869af152e
Pandatech2712 <144889346+Pandatech2712@users.noreply.github.com>
2024-02-25 17:42:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//FIT e-mail
+let jmeno = prompt ("Zadejte prosím vaše jméno (bez diakritiky):");
+let surname = prompt ("Zadejte prosím vaše přijmení (bez diakritiky):");
+
+//odstranění bílých míst
+jmeno.trim();
+surname.trim();
+
+//vytvoření emailu
+const prefixEmail = jmeno.slice(0,3) + surname.slice(0,3);
+const email = prefixEmail + "@fit.cvut.cz";
+
+document.body.innerHTML = "Váš e-mai je " + email
